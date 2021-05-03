bb7540c64a0b2ace0aa7115064ed3b1f9e74f228
Java-hsl <1358311815@qq.com>
2021-05-03 16:07:08 +0800

    注册zx

--- eurake/src/main/java/com/sell/eurake/EurakeApplication.java
+++ eurake/src/main/java/com/sell/eurake/EurakeApplication.java
@@ -2,8 +2,14 @@ package com.sell.eurake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * 这就是一个注册中心
+ * 既是Server，同时本身也是一个Clent端
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class EurakeApplication {
 
     public static void main(String[] args) {
